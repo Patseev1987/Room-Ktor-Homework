ae3fd0578a3275db4c204af48bb0cb4d135c1922
Bogdan Patseev <patseev1987@gmail.com>
2025-07-17 19:37:24 +0300

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+//plugins {
+//    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+//}
+
+dependencyResolutionManagement {
+   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories{
+        google()
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+
+rootProject.name = "storage"
+rootProject.name = "Room-Ktor-Homework"
